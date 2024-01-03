d8ecc17dca04235063857d5059dd0de4896a71c9
henok3878 <henokwondimu3878@gmail.com>
2024-01-03 22:05:48 +0300

    Add Firebase Core and Firestore packages with config integration

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,14 @@ import 'package:habo/notifications.dart';
 import 'package:habo/settings/settings_manager.dart';
 import 'package:provider/provider.dart';
 import 'package:window_size/window_size.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   addLicenses();
   runApp(
     const Habo(),
