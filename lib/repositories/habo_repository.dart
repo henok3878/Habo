965d357397f47005b98739c24b30f5afbb43c4c3
henok3878 <henokwondimu3878@gmail.com>
2024-01-04 15:27:45 +0300

    Optimize UI Responsiveness with Asynchronous Remote Updates

--- lib/repositories/habo_repository.dart
+++ lib/repositories/habo_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:habo/habits/habit.dart';
 import 'package:habo/repositories/habo_repo_interface.dart';
 
@@ -19,25 +20,37 @@ class HaboRepository implements HaboRepoInterface {
   @override
   Future<void> clearDatabase() async {
     await _localRepo.clearDatabase();
-    await _localRepo.clearDatabase();
+    // Initiate the remote repository update in the background
+    _localRepo.clearDatabase().catchError((error) {
+      debugPrint("Error clearing remote db: $error");
+    });
   }
 
   @override
   Future<void> deleteEvent(String id, DateTime dateTime) async {
     await _localRepo.deleteEvent(id, dateTime);
-    await _remoteRepo.deleteEvent(id, dateTime);
+    // Initiate the remote repository update in the background
+    await _remoteRepo.deleteEvent(id, dateTime).catchError((error) {
+      debugPrint("Error deleting event from remote repository: $error");
+    });
   }
 
   @override
   Future<void> deleteHabit(String id) async {
     await _localRepo.deleteHabit(id);
-    await _remoteRepo.deleteHabit(id);
+    // Initiate the remote repository update in the background
+    _remoteRepo.deleteHabit(id).catchError((error) {
+      debugPrint("Error deleting habit from remote repository: $error");
+    });
   }
 
   @override
   Future<void> editHabit(Habit habit) async {
     await _localRepo.editHabit(habit);
-    await _remoteRepo.editHabit(habit);
+    // Initiate the remote repository update in the background
+    _remoteRepo.editHabit(habit).catchError((error) {
+      debugPrint("Error editing habit to remote repository: $error");
+    });
   }
 
   @override
@@ -55,28 +68,29 @@ class HaboRepository implements HaboRepoInterface {
   @override
   Future<void> insertEvent(String id, DateTime date, List event) async {
     await _localRepo.insertEvent(id, date, event);
-    await _remoteRepo.insertEvent(id, date, event);
+
+    // Initiate the remote repository update in the background
+    _remoteRepo.insertEvent(id, date, event).catchError((error) {
+      debugPrint("Error inserting event to remote repository: $error");
+    });
   }
 
   @override
   Future<void> insertHabit(Habit habit) async {
     await _localRepo.insertHabit(habit);
-    await _remoteRepo.insertHabit(habit);
-    // // Initiate the remote repository update in the background
-    // _remoteRepo.insertHabit(habit).catchError((error) {
-    //   debugPrint("Error updating remote repository: $error");
-    // });
+    // Initiate the remote repository update in the background
+    _remoteRepo.insertHabit(habit).catchError((error) {
+      debugPrint("Error updating remote repository: $error");
+    });
   }
 
   @override
   Future<void> updateOrder(List<Habit> habits) async {
     await _localRepo.updateOrder(habits);
-    await _remoteRepo.updateOrder(habits);
-
-    // // Initiate the remote repository update in the background
-    // _remoteRepo.updateOrder(habits).catchError((error) {
-    //   debugPrint("Error updating order remote repository: $error");
-    // });
+    // Initiate the remote repository update in the background
+    _remoteRepo.updateOrder(habits).catchError((error) {
+      debugPrint("Error updating order remote repository: $error");
+    });
   }
 
   @override
