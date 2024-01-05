bc44143368c4e0675bd6e2049f42492e7a59463c
henok3878 <henokwondimu3878@gmail.com>
2024-01-05 23:11:23 +0300

    Integrate UserRepository into Dependency Injector

--- lib/utils/dependency_injector.dart
+++ lib/utils/dependency_injector.dart
@@ -1,27 +1,35 @@
 import 'package:habo/repositories/habo_local_repo.dart';
 import 'package:habo/repositories/habo_remote_repo.dart';
 import 'package:habo/repositories/habo_repository.dart';
+import 'package:habo/repositories/user_repository.dart';
 
 class DependencyInjector {
   static HaboLocalRepository? _haboLocalRepository;
   static HaboRemoteRepository? _haboRemoteRepository;
   static HaboRepository? _haboRepository;
+  static UserRepository? _userReposirory;
 
-  static Future<HaboLocalRepository> getHaboLocalRepository() async{
+  static Future<HaboLocalRepository> getHaboLocalRepository() async {
     _haboLocalRepository ??= await HaboLocalRepository.create();
     return _haboLocalRepository!;
   }
 
-  static HaboRemoteRepository getHaboRemoteRepository() {
-    _haboRemoteRepository ??= HaboRemoteRepository();
+  static Future<HaboRemoteRepository> getHaboRemoteRepository() async {
+    _haboRemoteRepository ??=
+        HaboRemoteRepository(userRepository: await getUserRepository());
     return _haboRemoteRepository!;
   }
 
   static Future<HaboRepository> getHaboRepository() async {
     _haboRepository ??= HaboRepository(
         localRepo: await getHaboLocalRepository(),
-        remoteRepo: getHaboRemoteRepository());
+        remoteRepo: await getHaboRemoteRepository());
 
     return _haboRepository!;
+  }
+
+  static Future<UserRepository> getUserRepository() async {
+    _userReposirory ??= UserRepository();
+    return _userReposirory!;
   }
 }
