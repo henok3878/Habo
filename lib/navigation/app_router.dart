79bafcaa4d5f88503095a7bb80dd86cc8be52017
henok3878 <henokwondimu3878@gmail.com>
2024-01-05 22:58:16 +0300

    Update AppRouter for sign-in and sign-up navigation

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -1,4 +1,8 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
+import 'package:habo/auth/auth_manager.dart';
+import 'package:habo/auth/sign_in_screen.dart';
+import 'package:habo/auth/sign_up_screen.dart';
 import 'package:habo/habits/edit_habit_screen.dart';
 import 'package:habo/habits/habits_manager.dart';
 import 'package:habo/habits/habits_screen.dart';
@@ -18,15 +22,18 @@ class AppRouter extends RouterDelegate
   final AppStateManager appStateManager;
   final SettingsManager settingsManager;
   final HabitsManager habitsManager;
+  final AuthManager authManager;
 
   AppRouter(
       {required this.appStateManager,
       required this.settingsManager,
-      required this.habitsManager})
+      required this.habitsManager,
+      required this.authManager})
       : navigatorKey = GlobalKey<NavigatorState>() {
     appStateManager.addListener(notifyListeners);
     settingsManager.addListener(notifyListeners);
     habitsManager.addListener(notifyListeners);
+    authManager.addListener(notifyListeners);
     settingsManager.getSeenOnboarding;
   }
 
@@ -35,30 +42,58 @@ class AppRouter extends RouterDelegate
     appStateManager.removeListener(notifyListeners);
     settingsManager.removeListener(notifyListeners);
     habitsManager.removeListener(notifyListeners);
+    authManager.removeListener(notifyListeners);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Navigator(
-      key: navigatorKey,
-      onPopPage: _handlePopPage,
-      pages: [
-        if (allInitialized()) HabitsScreen.page(),
-        if (appStateManager.getStatistics) StatisticsScreen.page(),
-        if (appStateManager.getSettings) SettingsScreen.page(),
-        if (appStateManager.getOnboarding || !settingsManager.getSeenOnboarding)
-          OnboardingScreen.page(),
-        if (appStateManager.getCreateHabit) EditHabitScreen.page(null),
-        if (appStateManager.getEditHabit != null)
-          EditHabitScreen.page(appStateManager.getEditHabit!),
-        if (!allInitialized()) SplashScreen.page(),
-      ],
-    );
+        key: navigatorKey, onPopPage: _handlePopPage, pages: getPages(context));
+  }
+
+  List<Page> getPages(context) {
+    List<Page> pages = [];
+    if (!allInitialized()) {
+      pages.add(SplashScreen.page());
+    }
+    if (authManager.status == Status.authenticated) {
+      if (allInitialized()) {
+        pages.add(HabitsScreen.page());
+      }
+      if (appStateManager.getStatistics) {
+        pages.add(StatisticsScreen.page());
+      }
+      if (appStateManager.getSettings) {
+        pages.add(SettingsScreen.page());
+      }
+      if (appStateManager.getCreateHabit) {
+        pages.add(EditHabitScreen.page(null));
+      }
+      if (appStateManager.getEditHabit != null) {
+        pages.add(EditHabitScreen.page(appStateManager.getEditHabit!));
+      }
+    } else {
+      if (appStateManager.getSignUp ||
+          authManager.status == Status.registering) {
+        pages.add(SignUpScreen.page());
+      } else if (appStateManager.getSignIn ||
+          authManager.status == Status.authenticating ||
+          settingsManager.getSeenOnboarding) {
+        pages.add(SignInScreen.page());
+      }
+    }
+    if (appStateManager.getOnboarding || !settingsManager.getSeenOnboarding) {
+      pages.add(OnboardingScreen.page());
+    }
+
+    return pages;
   }
 
   bool allInitialized() {
-    return settingsManager.isInitialized && habitsManager.isInitialized;
+    return settingsManager.isInitialized &&
+        habitsManager.isInitialized &&
+        authManager.isInitialized;
   }
 
   bool _handlePopPage(Route<dynamic> route, result) {
@@ -84,6 +119,12 @@ class AppRouter extends RouterDelegate
 
     if (route.settings.name == Routes.editHabitPath) {
       appStateManager.goEditHabit(null);
+    }
+    if (route.settings.name == Routes.signInPath) {
+      appStateManager.goSignIn(false);
+    }
+    if (route.settings.name == Routes.signUpPath) {
+      appStateManager.goSignUp(false);
     }
 
     return false;
